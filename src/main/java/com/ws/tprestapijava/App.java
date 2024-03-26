eb15722eaa2f0034fa65c91606fb28ef31fec259
Lenovo <mouniri.ouma@gmail.com>
2024-03-26 01:58:48 +0000

    inital commit

--- src/main/java/com/ws/tprestapijava/App.java
+++ src/main/java/com/ws/tprestapijava/App.java
@@ -1,2 +1,96 @@
-package com.ws.tprestapijava;public class App {
+package com.ws.tprestapijava;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.client.ClientConfig;
+
+public class App {
+    private static final String BASE_URI = "http://localhost:8080/TPrestAPI-1.0-SNAPSHOT/rest/products";
+    private final Client client;
+    public App() {
+        ClientConfig config = new ClientConfig();
+        this.client = ClientBuilder.newClient(config);
+    }
+    public void close() {
+        client.close();
+    }
+    public String getList() {
+        WebTarget target = client.target(BASE_URI);
+        return target.request().accept(MediaType.APPLICATION_JSON).get(String.class);
+    }
+    public Product getProduct(String productId) {
+        WebTarget target = client.target(BASE_URI + "/" + productId);
+        return target.request().accept(MediaType.APPLICATION_JSON).get(Product.class);
+    }
+    public String addProduct(Product product) {
+        WebTarget target = client.target(BASE_URI);
+        Response response = target.request()
+                .post(Entity.entity(product, MediaType.APPLICATION_JSON), Response.class);
+        String location = response.getLocation().toString();
+        String id = String.valueOf(product.getId());
+        return BASE_URI + "/" + id;
+    }
+    public Response updateProduct(String productId, Product product) {
+        WebTarget target = client.target(BASE_URI).path(productId);
+        return target.request()
+                .put(Entity.entity(product, MediaType.APPLICATION_JSON), Response.class);
+    }
+    public Response deleteProduct(String productId) {
+        WebTarget target = client.target(BASE_URI).path(productId);
+        return target.request().delete(Response.class);
+    }
+
+    public static void main(String[] args) {
+        App restClient = new App();
+        try {
+            restClient.testMethods();
+        } finally {
+            restClient.close();
+        }
+    }
+
+    private void testMethods() {
+        // Test list method
+        System.out.println("List of products:");
+        System.out.println(getList());
+
+        System.out.println("-----------------------------");
+
+        // Test get method
+        System.out.println("Product with ID 3:");
+        System.out.println(getProduct("3"));
+
+        System.out.println("-----------------------------");
+
+        // Test add method
+        Product newProduct = new Product(4, "phone ouma", 6000, "samsung A52s");
+        System.out.println("Added product location:");
+        System.out.println(addProduct(newProduct));
+        System.out.println("List of products:");
+        System.out.println(getList());
+
+
+        System.out.println("-----------------------------");
+
+        // Test update method
+        System.out.println("Product with ID 4:");
+        System.out.println(getProduct("4"));
+        Product updatedProduct = new Product(4, "Chareur", 300, "Chargeur type C 5m");
+        System.out.println("Update product response:");
+        System.out.println(updateProduct("4", updatedProduct));
+        System.out.println("Product with ID 4:");
+        System.out.println(getProduct("4"));
+
+        System.out.println("-----------------------------");
+
+        // Test delete method
+        System.out.println("Delete product response:");
+        System.out.println(deleteProduct("4"));
+        System.out.println("List of products:");
+        System.out.println(getList());
+    }
 }
--- src/main/java/com/ws/tprestapijava/Product.java
+++ src/main/java/com/ws/tprestapijava/Product.java
@@ -1,2 +1,87 @@
-package com.ws.tprestapijava;public class Product {
+package com.ws.tprestapijava;
+
+public class Product {
+    private int id;
+    private String label;
+    private float price;
+    private String description;
+
+    public Product(int id) {
+        this.id = id;
+    }
+
+    public Product() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Product(int id, String name, float price, String description) {
+        this.id = id;
+        this.label = name;
+        this.price = price;
+        this.description = description;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", label='" + label + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 }
